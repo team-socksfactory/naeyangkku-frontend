939085db957353384f533b124ef2f9c22ca0b8d5
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-06 15:04:12 +0900

    feat :: 벽난로꾸미기Modal

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import * as S from "./style";
-import { NaeYangKkuTheme } from "src/style/theme";
+import React from 'react';
+import * as S from './style';
+import { NaeYangKkuTheme } from 'src/style/theme';
+import Modal from './Modal';
 
 const Home = () => {
   return (
     <S.MainWrap>
       <S.TitleWrap>
-        <div style={{ display: "inline-flex", gap: "3px" }}>
+        <div style={{ display: 'inline-flex', gap: '3px' }}>
           <h1 style={{ color: `${NaeYangKkuTheme.secondlyNormal}` }}>루돌돌</h1>
           <h1>님의 벽난로</h1>
         </div>
         <div>
-          <span style={{ fontSize: "16px" }}>12</span>
+          <span style={{ fontSize: '16px' }}>12</span>
           <span>개의 양말이 도착했어요!</span>
         </div>
       </S.TitleWrap>
       <S.SocksWrap></S.SocksWrap>
       <S.Button>벽난로 꾸며주기</S.Button>
+      <Modal />
     </S.MainWrap>
   );
 };
