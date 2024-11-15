3da28b3a429cfbd438f1760f52beafa952b13956
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-15 11:41:09 +0900

    feat :: OnBoarding

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signin from '../Auth/Signin';
 import Home from '../Home';
+import OnBoarding from '../Common/OnBoarding';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/sign" element={<Signin />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<OnBoarding />} />
+        <Route path="*" element={<>404</>} />
       </Routes>
     </BrowserRouter>
   );
