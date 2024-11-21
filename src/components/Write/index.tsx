a4fbe221e0147ea45133c17038440571148474cb
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-11-21 23:31:26 +0900

    feat :: WriteView

---
+++ src/components/Write/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import * as S from "./style";
+import SantaImage from "src/assets/img/write/santa.svg";
+import {contentInput, NameInput, NameWrap} from "./style";
+
+const Write: React.FC = () => {
+    return (
+        <S.MainWrap>
+            <S.ProgressBar>
+                <div className="Progress"></div>
+                <S.ProgressText>60%</S.ProgressText>
+            </S.ProgressBar>
+            <S.TitleWrap>
+                <h1>메세지를 남겨주세요!</h1>
+            </S.TitleWrap>
+            <S.BackWrap>
+                <S.IconImage src={SantaImage} alt="Message icon" />
+                <S.Frame>
+                    <S.InputWrap>
+                        <S.contentInput
+                            placeholder="마음을 담은 메세지를 입력해주세요."
+                        />
+                    </S.InputWrap>
+                </S.Frame>
+            </S.BackWrap>
+            <S.NameWrap>
+                <S.NameInput
+                    placeholder="남길 이름을 입력해주세요."
+                />
+            </S.NameWrap>
+            <S.ShowWrap>
+                <S.ShowSpan>미리 보기</S.ShowSpan>
+            </S.ShowWrap>
+        </S.MainWrap>
+    );
+}
+
+export default Write;
---
+++ src/components/Write/style.ts
@@ -0,0 +1,180 @@
+import styled from "styled-components";
+import LayoutImage from "src/assets/img/write/layout.svg";
+
+export const MainWrap = styled.div`
+    position: relative;
+    background-color: #3D3332;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    padding-top: 90px;
+`;
+
+export const ProgressBar = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 18px;
+    background: #211B1B;
+    display: flex;
+
+    .Progress {
+        width: 60%;
+        background: #FF7777;
+        height: 100%;
+        border-radius: 0 10px 10px 0;
+    }
+`;
+
+export const ProgressText = styled.span`
+    position: absolute;
+    bottom: -35px;
+    right: 7%;
+    color: #FFF;
+    font-size: 20px;
+    font-weight: bold;
+    font-family: "GangwonEduSaeeum OTF";
+`;
+
+export const TitleWrap = styled.div`
+    margin-bottom: 40px;
+    margin-right: 170px;
+
+    h1 {
+        color: #FFF;
+        font-size: 24px;
+        font-family: "GangwonEduSaeeum OTF";
+        font-style: normal;
+        font-weight: 700;
+        line-height: 22px;
+    }
+`;
+
+export const BackWrap = styled.div`
+    background-color: #F7EEE7;
+    width: 300px;
+    height: 330px;
+    padding: 20px;
+    border-radius: 28px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    position: relative;
+    //overflow: hidden;
+`;
+
+export const Frame = styled.div`
+    border: 1.6px dashed rgba(255, 119, 119, 0.8);
+    border-radius: 20px;
+    width: 322px;
+    height: 375px;
+`;
+
+export const IconImage = styled.img`
+    z-index: 10;
+    width: 70px;
+    height: 70px;
+    position: absolute; 
+    top: -35px; 
+    left: 50%;
+    transform: translateX(-50%);
+`;
+
+
+export const InputWrap = styled.div`
+    width: 100%;
+    margin-top: auto;
+    z-index: 1;
+`;
+
+export const contentInput = styled.textarea`
+    width: 90%;
+    height: auto;
+    min-height: 300px;
+    padding: 30px 0 0 0;
+    border: none;
+    background: transparent;
+    font-size: 20px;
+    font-family: "GangwonEduSaeeum OTF";
+    color: #3D3332;
+    box-sizing: border-box;
+    resize: none;
+    overflow-y: auto; 
+
+    &::placeholder {
+        font-size: 20px;
+        color: #A9A9A9;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+export const NameWrap = styled.div`
+    margin-top: 30px;
+    background-color: #F7EEE7;
+    width: 300px;
+    height: 15px;
+    padding: 20px;
+    border-radius: 28px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    position: relative;
+`;
+
+export const NameInput = styled.textarea`
+    width: 90%;
+    height: 20px;
+    text-align: center;
+    border: none;
+    background: transparent;
+    font-size: 20px;
+    font-family: "GangwonEduSaeeum OTF";
+    color: #3D3332;
+    box-sizing: border-box;
+    resize: none;
+    display: flex;
+    align-items: center; 
+    justify-content: center; 
+    padding: 0;
+    margin-bottom: -7px;
+
+    &::placeholder {
+        margin-bottom: -10px;
+        font-size: 20px;
+        color: #A9A9A9;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+export const ShowWrap = styled.div`
+    margin-top: 30px;
+    background-color: #FFFFFF;
+    width: 300px;
+    height: 20px;
+    top: 50px;
+    padding: 20px;
+    border-radius: 28px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center; 
+    align-items: center; 
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    position: relative;
+`;
+
+export const ShowSpan = styled.span`
+    color: #3D3332;
+    font-weight: bold;
+    font-size: 20px;
+`;
