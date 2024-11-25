01c9b396b540695024fe86cc16ef3463dfe3fe3c
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-25 12:36:54 +0900

    feat :: redirect

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Signin from '../Auth/Signin';
 import Home from '../Home';
 import DecorativePage from '../DecorativePage';
+import token from 'src/libs/token/token';
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from 'src/constants/token.constants';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {token.getToken(ACCESS_TOKEN_KEY) && token.getToken(REFRESH_TOKEN_KEY) ? (
+          <Route path="/:id" element={<Home />} />
+        ) : (
+          <Route path="/" element={<Navigate to="/sign" />} />
+        )}
+        <Route path="/" element={<Navigate to="/sign" />} />
         <Route path="/sign" element={<Signin />} />
         <Route path="/:id" element={<Home />} />
         <Route path="/decorativepage" element={<DecorativePage />} />
